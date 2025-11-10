2c959962563aff4c4c78d331ac99a5e8d7ba4d56
wedo <costadefelipe@gmail.com>
2025-11-10 15:20:01 -0300

    add new function selectProduct

--- cmd/crud/main.go
+++ cmd/crud/main.go
@@ -82,3 +82,21 @@ func updateProduct(db *sql.DB, product *Product) error {
 
 	return nil
 }
+
+func selectProduct(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select id, name, price from products where id=?")
+	if err != nil {
+		return nil, err
+	}
+
+	defer stmt.Close()
+	var p Product
+
+	err = db.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return nil, err
+	}
+
+	return &p, nil
+
+}
